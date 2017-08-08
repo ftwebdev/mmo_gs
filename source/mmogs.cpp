5835352c4872bbbcdf03eab5cf408a428a380e0d
felex67 <felexxx67@gmail.com>
2017-08-09 00:45:01 +0500

    several

--- source/mmogs.cpp
+++ source/mmogs.cpp
@@ -1,24 +1,24 @@
-#include "ftwd/mmogs.h"
-#include "tpl/global.h"
-#include <io.h>
-#include <Windows.h>
+//#include "ftwd/mmogs.h"
+//#include "tpl/global.h"
+#include <unistd.h>
+#include <time.h>
+//#include <Windows.h>
 #include <random>
 
 namespace ftwd {
 	void setOutputColor(const unsigned char _Text) {
-		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-		unsigned char txt = (_Text < 16 ? _Text : 15);
-		SetConsoleTextAttribute(handle, txt);
+		
 	}
 };
 
 int main(){
 	char test[] = "Hello World! ";
 	const char ret[] = " \r";
-	char spin[] = { "\\|/-\\|/-" };
+	char spin[] = { "\\|/|" };
 	size_t len = sizeof(test) - 1;
-	size_t slen = sizeof(spin - 1);
+	size_t slen = sizeof(spin) - 1;
 	size_t spos = 0;
+	struct timespec ts, tw = { 0, 500 };
 	while (true) {
 		if (spos == slen)
 			spos = 0;
@@ -27,7 +27,7 @@ int main(){
 		spos++;
 		write(1, ret, 1);
 		write(1, test, len);
-		Sleep(200);
+		nanosleep(&tw, &ts);
 	}
 #ifdef _WIN32
     system("pause");
