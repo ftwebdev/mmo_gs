7d3779595164606f8f95a5ba257a1b3ea22b6289
felex67 <felexxx67@gmail.com>
2017-08-20 09:29:40 +0500

    several

--- source/mmogs.cpp
+++ source/mmogs.cpp
@@ -1,20 +1,9 @@
 #include "ftwd/mmogs.h"
 #include <iostream>
-#include <io.h>
 
 int main(){
 	using namespace ftwd;
-	const char testMsg[] = { "Привет, Мир!\n" };
-	byte* test = (byte*)dcopy(testMsg);
-	size_t len = strlen(test);
-	encoding::convert(EncCP1251, EncUTF8, test, len, false);
-	FILE* file = fopen("enctest.txt", "wb");
-	if (!file) {
-		std::cout << "Can't open enctest.txt\n";
-	}
-	int fd = fileno(file);
-	write(fd, test, len);
-	fclose(file);
+	
 #ifdef _WIN32
     system("pause");
 #endif
